88e999ea51db8361f966a14602463236ae41ec55
Nick <NickyDaB@gmail.com>
2026-06-05 10:27:17 -0400

    Delete db-config.php

--- food-journal/api/db-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//Replace with credentials and do not track!
-//$DB_HOST = "YOUR_HOSTNAME_HERE";   // e.g. mysql.yourdomain.com (DreamHost shows this)
-//$DB_NAME = "YOUR_DB_NAME_HERE";
-//$DB_USER = "YOUR_DB_USER_HERE";
-//$DB_PASS = "YOUR_DB_PASSWORD_HERE";
