abff2a0cfcec9622db9f52671a02bd9c23ffa9be
Nick <NickyDaB@gmail.com>
2026-06-05 10:25:11 -0400

    Create db-config.example.php

---
+++ food-journal/api/db-config.example.php
@@ -0,0 +1,12 @@
+<?php
+
+// USE THIS FILE AS A TEMPLATE EXAMPLE
+// COPY PASTE AND RENAME AS "db-config.php" with proper credentials
+// DO NOT COMMIT credentials to repo
+// make sure the new file is in git ignore
+
+//Replace with credentials and do not track!
+//$DB_HOST = "YOUR_HOSTNAME_HERE";   // e.g. mysql.yourdomain.com (DreamHost shows this)
+//$DB_NAME = "YOUR_DB_NAME_HERE";
+//$DB_USER = "YOUR_DB_USER_HERE";
+//$DB_PASS = "YOUR_DB_PASSWORD_HERE";
